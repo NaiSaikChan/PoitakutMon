afeb39f54e90ab77a4230f74a0e0fe219bc6a8c2
Saik Chan <maraohnonpon@hotmail.com>
2024-08-04 10:35:55 +0700

    Initial Commit

---
+++ PoitakutMon/PoitakutMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoitakutMonApp.swift
+//  PoitakutMon
+//
+//  Created by Silarcara on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PoitakutMonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
